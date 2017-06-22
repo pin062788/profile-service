81708a2d7f990db4a036a26d588ba19cc7a59c45
Sourav Pati <sourav.pati09@gmail.com>
2017-06-21 19:41:54 -0500

    modified docker file

--- src/main/java/com/souravpati/profile/Application.java
+++ src/main/java/com/souravpati/profile/Application.java
@@ -1,13 +1,21 @@
 package com.souravpati.profile;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class Application
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
     }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
 }
