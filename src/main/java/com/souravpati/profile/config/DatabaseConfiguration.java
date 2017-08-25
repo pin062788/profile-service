816edd5bd4df648d790ead52ae9e3d25838f59d0
spati <spati@mbopartners.com>
2017-08-25 14:18:09 -0500

    Removed db configuration

--- src/main/java/com/souravpati/profile/config/DatabaseConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.souravpati.profile.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-
-@Configuration
-@EnableElasticsearchRepositories(basePackages = "com.souravpati.profile.repository")
-public class DatabaseConfiguration {
-}
